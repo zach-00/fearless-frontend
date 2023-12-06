bbd9cad85eb5e51d91cc8c520a11f12cf69d7942
Zach <walkowiakzach05@gmail.com>
2023-12-05 21:56:49 -0800

    Added dates to card footers

--- ghi/js/app.js
+++ ghi/js/app.js
@@ -1,11 +1,14 @@
 
-function createCard(name, description, pictureUrl) {
+function createCard(name, description, pictureUrl, startDate, endDate) {
     return `
     <div class="card shadow p-3 mb-5 bg-body-tertiary rounded">
       <img src="${pictureUrl}" class="card-img-top">
       <div class="card-body">
         <h5 class="card-title">${name}</h5>
         <p class="card-text">${description}</p>
+      </div>
+      <div class="card-footer">
+        <p>${startDate} - ${endDate}</p>
       </div>
     </div>
     `;
@@ -37,7 +40,15 @@ window.addEventListener('DOMContentLoaded', async () => {
                 const title = details.conference.name;
                 const description = details.conference.description;
                 const pictureUrl = details.conference.location.picture_url;
-                const html = createCard(title, description, pictureUrl);
+
+                const start = details.conference.starts;
+                const end = details.conference.ends;
+                const parsedStart = Date.parse(start);
+                const parsedEnd = Date.parse(end);
+                const startDate = new Date(parsedStart).toLocaleDateString();
+                const endDate = new Date(parsedEnd).toLocaleDateString();
+
+                const html = createCard(title, description, pictureUrl, startDate, endDate);
 
                 column[index].innerHTML += html;
                 if (index < 2) {
