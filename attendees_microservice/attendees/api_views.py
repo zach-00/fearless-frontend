2e78fce5cbcca5a2e97acbd9b5bb2b6ceaa38af5
Zach <walkowiakzach05@gmail.com>
2023-12-07 18:22:41 -0800

    Added first React Page

--- attendees_microservice/attendees/api_views.py
+++ attendees_microservice/attendees/api_views.py
@@ -15,6 +15,8 @@ class AttendeeListEncoder(ModelEncoder):
     model = Attendee
     properties = ["name"]
 
+    def get_extra_data(self, o):
+        return {"conference": o.conference.name}
 
 class AttendeeDetailEncoder(ModelEncoder):
     model = Attendee
@@ -56,7 +58,10 @@ def api_list_attendees(request, conference_vo_id=None):
     }
     """
     if request.method == "GET":
-        attendees = Attendee.objects.filter(conference=conference_vo_id)
+        if conference_vo_id is not None:
+            attendees = Attendee.objects.filter(conference=conference_vo_id)
+        else:
+            attendees = Attendee.objects.all()
         return JsonResponse(
             {"attendees": attendees},
             encoder=AttendeeListEncoder,
